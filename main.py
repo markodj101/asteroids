83b051dc89b5fc4e0700d7bbd891920f100ff9b3
markodj101 <markodj10@gmail.com>
2024-12-23 23:56:55 +0100

    now the main.py displeys the player

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    print("Starting asteroids!")
-    print("Screen width:",SCREEN_WIDTH)
-    print("Screen height:",SCREEN_HEIGHT)
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    while (True):
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+
+    while True:
           for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                       return
+                
+          screen.fill("black")
+          player.draw(screen)
+          
           pygame.display.flip()
+         
+          
+          # limit the framerates to 60 FPS
+          dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
 	main()
