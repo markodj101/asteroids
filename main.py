0a522c5b77e8b1b7c5fc675ea16f4804e48fccbf
markodj101 <markodj10@gmail.com>
2024-12-24 00:21:31 +0100

    player can now rotate

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x,y)
 
     while True:
-          for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                      return
-                
-          screen.fill("black")
-          player.draw(screen)
-          
-          pygame.display.flip()
-         
-          
-          # limit the framerates to 60 FPS
-          dt = clock.tick(60)/1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-	main()
-
+    main()
